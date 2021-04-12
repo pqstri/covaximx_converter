244ca3eeed9932160363c9d0dfbb00a30c9336bc
pqstri <lucacarmisciano@hotmail.com>
2021-04-12 11:50:46 +0200

    first load

---
+++ long_to_horiz.R
@@ -0,0 +1,77 @@
+require(tidyverse)
+
+# Import vertical data
+vert <- read.csv("Downloads/_CLINICAL_DATA.csv", sep = ";", na.strings = ".")
+
+trnsps <- function(vert) {
+  # Name patients
+  patient_db <- count(vert, PatientID) %>% select(-n)
+  
+  # Remove missing
+  vert <- filter(vert, !is.na(Value))
+  
+  # Check structure
+  # table(vert$VisitName)
+  # table(vert$FormName)
+  # table(vert$Question) 
+  # table(paste0(vert$VisitName, vert$FormName, vert$Question))
+  
+  # Generate univocal column name
+  # vert$instance_short <- if_else(vert$FormInstance == 1, "", as.character(vert$FormInstance), "")
+  vert_unic <- unite(vert, colname, VisitName, FormName, Question, FormInstance, sep = "___")
+  
+  # Keep only handy variables
+  vert_unic_simple <- select(vert_unic, PatientID, colname, DataType, Value) %>% 
+    mutate(., colname = str_remove(colname, "\\s*_{3}1$"))
+  
+  # Variable order
+  question_db <- select(vert_unic_simple, colname) %>% unique() %>% unlist()
+  
+  # Check structure
+  # table(vert$DataType) 
+  by_type_db <- split(as_tibble(vert_unic_simple), vert_unic_simple$DataType) %>% 
+    map(~ select(., -DataType))
+  
+  # Strings
+  string_db <- spread(by_type_db$String, colname, Value, convert = T)
+  
+  # Numerics
+  long_db <- spread(by_type_db$Long, colname, Value, convert = T)
+  float_db <- spread(by_type_db$Float, colname, Value, convert = T)
+  
+  #### Dates ####
+  
+  date_db <- mutate(by_type_db$Date, Value = as.Date(Value)) %>% 
+    spread(colname, Value)
+  
+  # remove col names to change
+  old_partialdate_question_db <- by_type_db$PartialDate$colname %>% unique() %>% unlist()
+  question_db <- setdiff(question_db, old_partialdate_question_db)
+  
+  # change format
+  by_type_db$PartialDate <- separate(by_type_db$PartialDate, Value, c("Year", "Month", "Day"), sep = "-") %>% 
+    gather("ymd", "value", Year, Month, Day) %>% 
+    unite(colname, colname, ymd, sep = "__")
+  
+  # store
+  partialdate_db <- spread(by_type_db$PartialDate, colname, value, convert = T)
+  
+  # add new col names
+  new_partialdate_question_db <- names(partialdate_db[-1])
+  question_db <- append(question_db, new_partialdate_question_db)
+  
+  #### Factors ####
+  list_db <- spread(by_type_db$List, colname, Value)
+  
+  #### Boolean ####
+  bool_db <- mutate(by_type_db$Boolean, Value = as.logical(Value)) %>% 
+    spread(colname, Value)
+  
+  # Gather all types
+  db_list <- list(patient_db, string_db, long_db, float_db, date_db, partialdate_db, list_db, bool_db)
+  
+  db <- purrr::reduce(db_list, left_join) %>% 
+    select(PatientID, all_of(question_db))
+  
+  return(db)
+}
